fc5c72a3947f1e42e9fe042e6ae512c0232fbac6
Holly <holly@Toms-MacBook-Pro.local>
2017-03-11 17:53:11 -0500

    Created registration component and route

---
+++ src/app/registration/registration.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.css']
+})
+export class RegistrationComponent implements OnInit {
+  checkboxTruthy = false;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  toggleCheckbox(){
+   this.checkboxTruthy = !this.checkboxTruthy;
+}
+
+}
